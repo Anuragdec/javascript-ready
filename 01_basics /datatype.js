b60f87fafef01db0b6a8ef6888187871363085a4
Anuragdec <125725130+Anuragdec@users.noreply.github.com>
2024-07-30 08:26:56 +0000

    pushing 3 files ...

---
+++ 01_basics /datatype.js
@@ -0,0 +1,12 @@
+"use strict " // code will treated as new by the engine
+
+let name= "anurag chaudhary"
+let password=8932
+let state=null
+let rank;
+
+console.log( typeof name)
+
+console.log( typeof rank)
+console.log( typeof password)
+console.log( typeof state)
---
+++ 01_basics /variables.js
@@ -0,0 +1,10 @@
+const empName = "Anurag"
+let empID= "55555"
+let empPassword="89989898"
+let empaccount;
+
+console.table([empName,empID,empPassword,empaccount])
+
+/*
+hum log var use ni krte bacause var me scope me problem hota he
+*/
