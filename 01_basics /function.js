ce38a111559739ecf910d4b536c5cc51119ac1b7
Anuragdec <125725130+Anuragdec@users.noreply.github.com>
2024-08-20 04:43:20 +0000

    two files funtion and scope is commit

---
+++ 01_basics /function.js
@@ -0,0 +1,25 @@
+/*function sum(num1,num2,...num3)
+{
+   
+   
+    console.log(num1 + num2)
+    return
+
+}
+
+
+console.log(sum(03 ,9,96,65,652))
+*/
+
+const emp={
+
+    name: "khushi",
+    salarys:120000
+}
+
+function handleemp(name,salary )
+{
+    console.log(name)
+    console.log(salary)
+}
+handleemp(emp.name,emp.salary)
---
+++ 01_basics /scope.js
@@ -0,0 +1,18 @@
+//there are two types of scope internal and gloabal scope lets see
+
+let name="anu"
+const age= 25
+var weight=600
+console.log(name)
+console.log(age)
+console.log(weight)
+
+
+
+if(true)
+{
+    var weight=60
+    console.log(age)
+    console.log(weight)
+    
+}
